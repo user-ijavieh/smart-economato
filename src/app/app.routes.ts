a41942659747e7a7fd097c7e4e12bd4ea747da92
user-ijavieh <jremedioscolmenares@gmail.com>
2026-04-16 10:04:19 +0100

    refactor(navegación): Movida funcionalidad de features a features/admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -91,7 +91,7 @@ export const routes: Routes = [
       {
         path: 'admin-panel/incidents',
         loadComponent: () =>
-          import('./features/incidents/incidents.component').then(m => m.IncidentsComponent),
+          import('./features/admin/incidents/incidents.component').then(m => m.IncidentsComponent),
         canActivate: [roleGuard('ADMIN')],
         data: { animation: 12 }
       },
@@ -153,7 +153,7 @@ export const routes: Routes = [
       {
         path: 'incidents',
         loadComponent: () =>
-          import('./features/incidents/incidents.component').then(m => m.IncidentsComponent),
+          import('./features/admin/incidents/incidents.component').then(m => m.IncidentsComponent),
         canActivate: [roleGuard('ADMIN', 'CHEF', 'ELEVATED')],
         data: { animation: 26 }
       },
