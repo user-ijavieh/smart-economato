a1601ef3e4d3d6bfa4adb1025f8678e63b2d3a50
user-ijavieh <jremedioscolmenares@gmail.com>
2026-04-23 07:43:42 +0100

    refactor(auth): Migrado de LocalStorage a SessionStorage

--- src/app/features/general/welcome/welcome.component.ts
+++ src/app/features/general/welcome/welcome.component.ts
@@ -7,6 +7,7 @@ import { ThemeService } from '../../../core/services/theme.service';
 import { NotificationService, SessionNotification } from '../../../core/services/notification.service';
 import { HttpQueryCacheService } from '../../../core/services/http-query-cache.service';
 import { MessageService } from '../../../core/services/message.service';
+import { StorageService } from '../../../core/services/storage.service';
 
 @Component({
   selector: 'app-welcome',
@@ -22,6 +23,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   private notificationService = inject(NotificationService);
   private httpQueryCacheService = inject(HttpQueryCacheService);
   private messageService = inject(MessageService);
+  private storageService = inject(StorageService);
 
   slides = [
     '/assets/img/carousel/carousel1.jpg',
@@ -88,7 +90,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   readonly isNotificationPanelOpen = signal(false);
   readonly isNotificationPanelRendered = signal(false);
   readonly isNotificationPanelClosing = signal(false);
-  readonly showUtilities = signal<boolean>(localStorage.getItem('welcome_utilities_visible') !== 'false');
+  readonly showUtilities = signal<boolean>(this.storageService.get('welcome_utilities_visible', 'local') !== 'false');
 
   logout(): void {
     if (this.isNotificationPanelOpen()) {
@@ -117,7 +119,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   toggleUtilities(): void {
     const newValue = !this.showUtilities();
     this.showUtilities.set(newValue);
-    localStorage.setItem('welcome_utilities_visible', String(newValue));
+    this.storageService.set('welcome_utilities_visible', String(newValue), 'local');
   }
 
   toggleNotificationPanel(event: MouseEvent): void {
